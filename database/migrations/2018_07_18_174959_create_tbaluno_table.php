d15f4962b757069ae5d12c33fabd754f3ae78a38
Myckol Fontes <myckol.ti@gmail.com>
2018-07-18 15:03:35 -0300

    enviando primeiros arquivos

---
+++ database/migrations/2018_07_18_174959_create_tbaluno_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateTbalunoTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('tbaluno', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('matricula', 30);
+            $table->string('name', 40);
+            $table->string('endereco', 100);
+            $table->string('bairro', 50);
+            $table->string('cep', 50);
+            $table->string('cidade', 50);
+            $table->string('uf', 50);
+            $table->string('email', 50);
+            $table->datetime('dtentrada', 50);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('tbaluno');
+    }
+}
